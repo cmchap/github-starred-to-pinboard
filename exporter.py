0efcd937c18032bbca3bbeefa73df050a6950a6f
cmchap <corymchapman@gmail.com>
2013-11-24 23:15:50 -0500

    changed tabs to spaces and started trying to get the readme text in the pinboard description

--- exporter.py
+++ exporter.py
@@ -15,7 +15,7 @@ tags = "github programming" #max of 100 tags, separated by spaces
 ## Functions ##
 ###############
 
-import requests, time, sys
+import requests, time, sys, base64
 
 def post_to_pinboard(pb_token, url, title, long_description, tags, replace):
 
@@ -30,40 +30,47 @@ def post_to_pinboard(pb_token, url, title, long_description, tags, replace):
     r = requests.get('https://api.pinboard.in/v1/posts/add', params=payload)
     r_status = r.status_code
     if r_status == 200:
-    	print "Added " + title
-    	return 1
+        print "Added " + title
+        return 1
     elif r_status == 403:
-    	print "Your Pinboard token didn't seem to work.\nYou should go get it from here: https://pinboard.in/settings/password"
-    	print "And paste it below.\nIt should look sorta like this: username:XXXXXXXXXXXXXXXXXXXX"
-    	pb_token = raw_input()
-    	return post_to_pinboard(pb_token, url, title, long_description, tags, replace)
+        print "Your Pinboard token didn't seem to work.\nYou should go get it from here: https://pinboard.in/settings/password"
+        print "And paste it below.\nIt should look sorta like this: username:XXXXXXXXXXXXXXXXXXXX"
+        pb_token = raw_input()
+        return post_to_pinboard(pb_token, url, title, long_description, tags, replace)
     elif r_status == 429:
-    	print "Whoa, Nellie! We're goin' too fast! Hold on, and we'll try again in a moment."
-    	time.sleep(3) # Pinboard API allows for 1 call every 3 seconds per user.
-    	return post_to_pinboard(pb_token, url, title, long_description, tags, replace)
+        print "Whoa, Nellie! We're goin' too fast! Hold on, and we'll try again in a moment."
+        time.sleep(3) # Pinboard API allows for 1 call every 3 seconds per user.
+        return post_to_pinboard(pb_token, url, title, long_description, tags, replace)
     else:
-    	print "Something went wrong while trying to bookmark " + title + ". I don't know what, but the http status code was " + r_status
-    	return 0
+        print "Something went wrong while trying to bookmark " + title + ". I don't know what, but the http status code was " + r_status
+        return 0
 
 def get_langs(langs_url, gh_token):
-	langs = ""
-	lang_data = requests.get("%s?access_token=%s" % (langs_url, gh_token))
-	if lang_data == "{}":
-		return langs
-	else:
-		lang_data = lang_data.json()
-		langs_sorted = sorted(lang_data.iteritems(), key=lambda bytes: -bytes[1]) #sort the languages into a list by most bytes.
-		for x in langs_sorted:
-		 	langs += "%s = %s bytes\n" % (x[0], x[1])
-		return langs
+    langs = ""
+    lang_data = requests.get("%s?access_token=%s" % (langs_url, gh_token))
+    if lang_data == "{}":
+        return langs
+    else:
+        lang_data = lang_data.json()
+        langs_sorted = sorted(lang_data.iteritems(), key=lambda bytes: -bytes[1]) #sort the languages into a list by most bytes.
+        for x in langs_sorted:
+            langs += "%s = %s bytes\n" % (x[0], x[1])
+        return langs
+
+def get_readme(api_url):
+    r = requests.get(api_url + "/readme")
+    if 300 >= r.status_code >=200:
+        return base64.b64decode(r.json()['content'])
+    else:
+        return "none listed"
 
 def test_token(url, token):
-	auth = {'auth_token': token}
-	r = requests.get(url, params=auth)
-	if r.status_code == 403:
-		return 0
-	else:
-		return 1
+    auth = {'auth_token': token}
+    r = requests.get(url, params=auth)
+    if r.status_code == 403:
+        return 0
+    else:
+        return 1
 
 
 ##############
@@ -80,23 +87,23 @@ url = 'https://api.github.com/users/' + gh_username + '/starred?page=1&per_page=
 #Test Github token
 token_tests = 3
 while test_token(url, gh_token) == 0:
-	if token_tests > 0:
-		token_tests  = token_tests - 1
-		print "Your Github token didn't seem to work. Can you try entering it in again?"
-		gh_token = raw_input()
-	else:
-		print "It seems like your token isn't working.\nYou can try running this script again, but the Github API may be down."
-		sys.exit()
+    if token_tests > 0:
+        token_tests  = token_tests - 1
+        print "Your Github token didn't seem to work. Can you try entering it in again?"
+        gh_token = raw_input()
+    else:
+        print "It seems like your token isn't working.\nYou can try running this script again, but the Github API may be down."
+        sys.exit()
 
 r = requests.get(url + "&access_token=" + gh_token)
 stars = r.json()
 while r.links: # iterate through the pages of github starred repos
-	if 'next' in r.links:
-		url = r.links['next']['url']
-		r = requests.get(url + "&access_token=" + gh_token)
-	        stars.extend(r.json())
-        else:
-            break
+    if 'next' in r.links:
+        url = r.links['next']['url']
+        r = requests.get(url + "&access_token=" + gh_token)
+        stars.extend(r.json())
+    else:
+        break
 
 #Pinboard
 print "Enter your Pinboard api token in the form username:XXXXXXXXXXXXXXXXXXXX\nYou can get it from here: https://pinboard.in/settings/password"
@@ -105,13 +112,13 @@ pb_token = raw_input()
 #Test Pinboard token
 token_tests = 3
 while test_token(url, pb_token) == 0:
-	if token_tests > 0:
-		token_tests  = token_tests - 1
-		print "Your Pinboard API token didn't seem to work. Can you try entering it in again?"
-		gh_token = raw_input()
-	else:
-		print "It seems like your token isn't working.\nYou can try running this script again, but the Pinboard API may be down."
-		sys.exit()
+    if token_tests > 0:
+        token_tests  = token_tests - 1
+        print "Your Pinboard API token didn't seem to work. Can you try entering it in again?"
+        gh_token = raw_input()
+    else:
+        print "It seems like your token isn't working.\nYou can try running this script again, but the Pinboard API may be down."
+        sys.exit()
 
 
 ###############
@@ -122,38 +129,44 @@ print "Adding your starred repos to Pinboard..."
 
 count = 0
 for item in range(len(stars)):
-	url = stars[item]['html_url']
-	name = stars[item]['name']
-	tagline = stars[item]['description']
+    repo_url = stars[item]['html_url']
+    name = stars[item]['name']
+    tagline = stars[item]['description']
+    repo_api_url = stars[item]['url']
 
-	#make title
-	title = name + ": " + tagline #max 255 characters according to the pinboard api.
+    #make title
+    title = name + ": " + tagline #max 255 characters according to the pinboard api.
 
-	# See if the homepage is listed.
-	page = stars[item]['homepage']
-	if page == False or page == None or page == "None" or page == "none" or page == "":
-		homepage = "none listed"
-	else:
-		homepage = str(page)
+    # See if the homepage is listed.
+    page = stars[item]['homepage']
+    if page == False or page == None or page == "None" or page == "none" or page == "":
+        homepage = "none listed"
+    else:
+        homepage = str(page)
 
-	#Make the programming languages of the repo in order of most bytes.
-	langs_url = stars[item]['languages_url']
-	langs = get_langs(langs_url, gh_token)
+    #Make the programming languages of the repo in order of most bytes.
+    langs_url = stars[item]['languages_url']
+    langs = get_langs(langs_url, gh_token) + "\n\n"
 
-	#Make the description
-	long_description = "This is a github repo. \nName: " + name
-	long_description += "\nTagline: " + tagline
-	if homepage != "none listed":
-	    long_description += "\nHomepage: " + homepage
-	if langs != []:
-	    long_description+= "\nLanguages:\n" + langs #max 65536 characters according to pinboard api.
+    #Make readme
+    readme = get_readme(repo_api_url)
 
-	pinboard_add = post_to_pinboard(pb_token, url, title, long_description, tags, replace)
-	if pinboard_add == 1:
-		count +=1
+    #Make the description. Max 65536 characters.
+    long_description = "Github repo \nName: " + name
+    long_description += "\nTagline: " + tagline
+    if homepage != "none listed":
+        long_description += "\nHomepage: " + homepage
+    if langs != []:
+        long_description += "\nLanguages:\n" + langs
+    if readme != "none listed":
+        long_description += readme
+
+    pinboard_add = post_to_pinboard(pb_token, repo_url, title, long_description, tags, replace)
+    if pinboard_add == 1:
+        count +=1
 if count == 0:
-	print "Whoopsh. Something went wrong, so we didn't add anything to your Pinboard."
+    print "Whoopsh. Something went wrong, so we didn't add anything to your Pinboard."
 elif count == 1:
-	print "You're all done. You only had one starred repo, so we added that to Pinboard. Go star more repos!"
+    print "You're all done. You only had one starred repo, so we added that to Pinboard. Go star more repos!"
 elif count > 1:
-	print "You're all done. All " + str(count) + " repos above have been added to pinboard!"
+    print "You're all done. All " + str(count) + " repos above have been added to pinboard!"
