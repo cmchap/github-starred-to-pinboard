6c65ce102eaadd108d11ed5411c9f57f8b046f56
cmchap <corymchapman@gmail.com>
2014-01-23 20:27:15 -0500

    config file and skipping work.

--- exporter.py
+++ exporter.py
@@ -64,17 +64,11 @@ def get_langs(langs_url, gh_token):
         return langs
 
 def get_readme(api_url, gh_token):
-#def get_readme(gh_username, name):
     r = requests.get(api_url + "/readme?access_token=" + gh_token)
-    readme_b64 = r.json()['content']
-    readme = base64.b64decode(readme_b64)
-    #readme = readme.encode(encoding='ascii', errors='ignore')
-    #r = requests.get("https://raw.github.com/" + gh_username + "/" + name + "/master/readme.md")
-    if 300 > r.status_code >=200:
-        # print r.content
-        # return r.content
-        #print readme
-        return readme
+    if r.status_code == 200:
+        readme_b64 = r.json()['content']
+        readme = base64.b64decode(readme_b64)
+        return str(readme)
     else:
         return "none listed"
 
@@ -171,6 +165,20 @@ def get_pinboard_token(sleep=1):
         return pb_token
 
 
+def get_current_from_pinboard(pb_token, tags):
+    payload = {
+    'auth_token': pb_token,
+    'tag': tags,
+    'format': 'json'
+    }
+    r = requests.get('https://api.pinboard.in/v1/posts/all', params=payload)
+    if r.status_code == 200:
+        bookmarks = r.json()
+        return bookmarks
+    else:
+        print "Something went wrong while trying to get bookmarks. The status code was " + str(r.status_code)
+        sys.exit()
+
 ##############
 ## Get info ##
 ##############
@@ -189,6 +197,15 @@ pb_token = get_pinboard_token()
 ## Main loop ##
 ###############
 
+
+# get existing bookmarks from pinboard
+existing = {}
+for bookmark in get_current_from_pinboard(pb_token, tags):
+    existing[bookmark['href']] = True
+print str(len(existing)) + " existing bookmarks found"
+
+
+# get stars from github
 url = 'https://api.github.com/users/' + gh_username + '/starred?page=1&per_page=100'
 
 r = requests.get(url + "&access_token=" + gh_token)
@@ -204,24 +221,43 @@ while r.links: # iterate through the pages of github starred repos
 print "Adding your starred repos to Pinboard..."
 
 count = 0
-for item in range(len(stars)):
-    repo_url = stars[item]['html_url']
-    name = stars[item]['name']
-    tagline = stars[item]['description']
-    repo_api_url = stars[item]['url']
+for star in stars:
+    repo_url = star['html_url']
+    if repo_url == False or repo_url == None or repo_url == "None" or repo_url == "none" or repo_url == "" or repo_url == "null":
+        repo_url == ""
+    name = star['name']
+    if name == False or name == None or name == "None" or name == "none" or name == "" or name == "null":
+        name = ""
+
+    # Skip existing starred repos
+    if replace == "no":
+        if repo_url in existing:
+            print "Skipping " + name
+            print repo_url
+            continue # breaks out of the for loop that iterates through the stars.
+
+    tagline = star['description']
+    if tagline == False or tagline == None or tagline == "None" or tagline == "none" or tagline == "" or tagline == "null":
+        tagline = ""
+    repo_api_url = star['url']
+    if repo_api_url == False or repo_api_url == None or repo_api_url == "None" or repo_api_url == "none" or repo_api_url == "" or repo_api_url == "null":
+        repo_api_url = ""
 
     #make title
-    title = name + ": " + tagline #max 255 characters according to the pinboard api.
+    if tagline == "":
+        title = name
+    else:
+        title = name + ": " + tagline #max 255 characters according to the pinboard api.
 
     # See if the homepage is listed.
-    page = stars[item]['homepage']
-    if page == False or page == None or page == "None" or page == "none" or page == "":
+    page = star['homepage']
+    if page == False or page == None or page == "None" or page == "none" or page == "" or page == "null":
         homepage = "none listed"
     else:
         homepage = str(page)
 
     #Make the programming languages of the repo in order of most bytes.
-    langs_url = stars[item]['languages_url']
+    langs_url = star['languages_url']
     langs = get_langs(langs_url, gh_token) + "\n\n"
 
     #Make readme
@@ -235,7 +271,7 @@ for item in range(len(stars)):
     if langs != []:
         long_description += "\nLanguages:\n" + langs
     if readme != "none listed":
-        long_description = long_description.encode('UTF-8', 'ignore')
+        long_description = long_description.encode('UTF-8','ignore')
         long_description += readme
 
     #test string lengths.
@@ -253,4 +289,4 @@ if count == 0:
 elif count == 1:
     print "You're all done. You only had one starred repo, so we added that to Pinboard. Go star more repos!"
 elif count > 1:
-    print "You're all done. All " + str(count) + " repos above have been added to pinboard!"
+    print "You're all done. " + str(count) + " repos have been added to pinboard!"
